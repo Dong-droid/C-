1ec4f135cd09975e384bcf787e81dc7ace99f875
Dong-droid <80375422+Dong-droid@users.noreply.github.com>
2021-07-20 17:33:48 +0900

    Update container.cpp

--- container.cpp
+++ container.cpp
@@ -188,3 +188,37 @@ int main() {
 	cin >> a>>b;
     cout<<max(rev(a),rev(b)); 
 }
+//정렬
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+    import java.util.Scanner;
+    
+    public class 단순계산기 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		//Scanner sc=new Scanner(System.in);int n=sc.nextInt();
+		int n=Integer.parseInt(br.readLine());
+		int [] ar=new int[n];
+		for(int i=0;i<n;i++)
+			ar[i]=Integer.parseInt(br.readLine());
+       br.close();
+		Arrays.sort(ar);
+		int c=0;
+		while(n>2) {
+			c=ar[n-1];
+			for(int i=n-3;i>=0&&i<n-2;i--) {
+				for(int j=n-2;j>i&&j<n-1;j--) {
+					if((ar[i]+ar[j]>c)||(ar[i]==c&&ar[j]==c)) {			
+						System.out.println(ar[i]+ar[j]+c);
+						return;
+					}
+					}
+				break;
+				}			
+		n--;	
+		}		
+	System.out.println(-1);
+	}			
+	}	
